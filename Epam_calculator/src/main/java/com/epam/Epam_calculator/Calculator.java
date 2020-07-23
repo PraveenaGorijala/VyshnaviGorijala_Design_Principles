bd20587a42a5f0ce27803bc655e01c01d2c91137
Vyshnavi Gorijala <43717995+PraveenaGorijala@users.noreply.github.com>
2020-07-23 17:12:33 +0530

    Add files via upload

---
+++ Epam_calculator/src/main/java/com/epam/Epam_calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.epam.Epam_calculator;
+
+public class Calculator {
+	double add(double x, double y) {
+		return x + y;
+	}
+	double subtract(double x, double y) {
+		return x - y;
+	}
+	double multiply(double x, double y) {
+		return x * y;
+	}
+	double divide(double x, double y) {
+		return x / y;
+	}
+}
